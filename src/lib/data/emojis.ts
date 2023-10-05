4705cf2da8d5a5f68a695702e2abd2e6d02a8759
Maya Avendano <mayari.avendano@gmail.com>
2023-10-05 23:40:03 +0100

    slack cats v1

---
+++ src/lib/data/emojis.ts
@@ -0,0 +1,292 @@
+import catAhhh from '$lib/assets/emojis/cat-ahh.jpg';
+import catBloop from '$lib/assets/emojis/cat-bloop.jpg';
+import catContent from '$lib/assets/emojis/cat-content.jpg';
+import catCowboy from '$lib/assets/emojis/cat-cowboy.jpg';
+import catHeheh from '$lib/assets/emojis/cat-heheh.jpg';
+import pushingSillyButtons from '$lib/assets/emojis/pushing-silly-buttons.jpg';
+import catScrunkly from '$lib/assets/emojis/cat-scrunkly.jpg';
+import meowChemicalRomance from '$lib/assets/emojis/meow-chemical-romance.png';
+import catWeirdlyHappy from '$lib/assets/emojis/cat-weirdly-happy.png';
+import catHeart from '$lib/assets/emojis/cat-heart.png';
+import mergeConflict from '$lib/assets/emojis/merge-conflict.png';
+import catDissociate from '$lib/assets/emojis/cat-dissociate.png';
+import catLoading from '$lib/assets/emojis/cat-loading.png';
+import catBanana from '$lib/assets/emojis/cat-banana.png';
+import catBeveragino from '$lib/assets/emojis/cat-beveragino.png';
+import catPoint from '$lib/assets/emojis/cat-point.png';
+import catConfused from '$lib/assets/emojis/cat-confused.png';
+import catAhhIntensified from '$lib/assets/emojis/cat-ahh-intensified.png';
+import catSad from '$lib/assets/emojis/cat-sad.png';
+import catCloseup from '$lib/assets/emojis/cat-closeup.png';
+import catThumb from '$lib/assets/emojis/cat-thumb.png';
+import catSleepyEepy from '$lib/assets/emojis/cat-sleepy-eepy.png';
+import catJudge from '$lib/assets/emojis/cat-judge.png';
+import catSpoopy from '$lib/assets/emojis/cat-spoopy.png';
+import catPeek from '$lib/assets/emojis/cat-peek.png';
+import catEepy from '$lib/assets/emojis/cat-eepy.png';
+import catHaha from '$lib/assets/emojis/cat-haha.png';
+import catGoggles from '$lib/assets/emojis/cat-goggles.png';
+import catHehe from '$lib/assets/emojis/cat-hehe.png';
+import catFabulous from '$lib/assets/emojis/cat-fabulous.jpg';
+import bicepCurl from '$lib/assets/emojis/bicep-curl.jpg';
+import gettingSwole from '$lib/assets/emojis/getting-swole.jpg';
+import notAmused from '$lib/assets/emojis/not-amused.jpg';
+import uWotM8 from '$lib/assets/emojis/u-wot-m8.jpg';
+import catAdmire from '$lib/assets/emojis/cat-admire.jpg';
+import catWFH from '$lib/assets/emojis/cat-WFH.jpg';
+import chefCat from '$lib/assets/emojis/chef-cat.jpeg';
+import catStare from '$lib/assets/emojis/cat-stare.jpg';
+import catHeck from '$lib/assets/emojis/cat-heck.jpg';
+import catAngry from '$lib/assets/emojis/cat-angry.jpg';
+import catCozy from '$lib/assets/emojis/cat-cozy.jpg';
+import catMechanic from '$lib/assets/emojis/cat-mechanic.jpg';
+import sousChef from '$lib/assets/emojis/sous-chef.jpg';
+import catInspect from '$lib/assets/emojis/cat-inspect.jpg';
+import catObserve from '$lib/assets/emojis/cat-observe.jpg';
+import catShook from '$lib/assets/emojis/cat-shook.jpg';
+import senorCheeto from '$lib/assets/emojis/senor-cheeto.jpg';
+
+export type Emoji = {
+	name: string;
+	alt: string;
+	src: string;
+};
+
+export const emojis: Emoji[] = [
+	{
+		name: 'cat-goggles',
+		alt: 'cat with goggles',
+		src: catGoggles
+	},
+	{
+		name: 'cat-hehe',
+		alt: 'cat with paw in face laughing',
+		src: catHehe
+	},
+
+	{
+		name: 'cat-haha',
+		alt: 'cat pointing and laughing',
+		src: catHaha
+	},
+	{
+		name: 'cat-eepy',
+		alt: 'a sleepy crinkly looking cat',
+		src: catEepy
+	},
+	{
+		name: 'cat-peek',
+		alt: 'a cat peeking over the camera',
+		src: catPeek
+	},
+	{
+		name: 'cat-spoopy',
+		alt: 'cat that looks like a demon',
+		src: catSpoopy
+	},
+	{
+		name: 'cat-judge',
+		alt: 'a judgemental cat',
+		src: catJudge
+	},
+	{
+		name: 'cat-sleepy-eepy',
+		alt: 'a sleepy cat',
+		src: catSleepyEepy
+	},
+	{
+		name: 'cat-thumb',
+		alt: 'a cat with a thumbs up',
+		src: catThumb
+	},
+	{
+		name: 'cat-closeup',
+		alt: 'a cat really close to the camera',
+		src: catCloseup
+	},
+	{
+		name: 'cat-sad',
+		alt: 'a cat with a sad face',
+		src: catSad
+	},
+	{
+		name: 'cat-ahh-intensified',
+		alt: 'a cat screaming',
+		src: catAhhIntensified
+	},
+	{
+		name: 'cat-confused',
+		alt: 'a confused cat',
+		src: catConfused
+	},
+	{
+		name: 'cat-point',
+		alt: 'a serious cat pointing at you',
+		src: catPoint
+	},
+	{
+		name: 'cat-beveragino',
+		alt: 'a cat with a beer and dumplings',
+		src: catBeveragino
+	},
+	{
+		name: 'cat-banana',
+		alt: 'a silly cat with a banana on its head',
+		src: catBanana
+	},
+	{
+		name: 'cat-loading',
+		alt: 'a cat with a loading wheel on its head',
+		src: catLoading
+	},
+	{
+		name: 'cat-dissociate',
+		alt: 'a cat dissociating',
+		src: catDissociate
+	},
+	{
+		name: 'merge-conflict',
+		alt: 'a cat in front of a laptop resolving a painful merge conflict',
+		src: mergeConflict
+	},
+	{
+		name: 'cat-heart',
+		alt: 'a cat with a korean finger heart',
+		src: catHeart
+	},
+	{
+		name: 'cat-weirdly-happy',
+		alt: 'a cat with a gummy smile',
+		src: catWeirdlyHappy
+	},
+	{
+		name: 'cat-ahhh',
+		alt: 'a cat screaming',
+		src: catAhhh
+	},
+	{
+		name: 'cat-bloop',
+		alt: 'a cat poking its tongue out',
+		src: catBloop
+	},
+	{
+		name: 'cat-content',
+		alt: 'a happy cat',
+		src: catContent
+	},
+	{
+		name: 'cat-cowboy',
+		alt: 'a large cat with a comically small cowboy hat',
+		src: catCowboy
+	},
+	{
+		name: 'cat-heheh',
+		alt: 'a cheeky cat that looks like he has a masterful plan',
+		src: catHeheh
+	},
+	{
+		name: 'pushing-silly-buttons',
+		alt: 'a cat typing on a keyboard',
+		src: pushingSillyButtons
+	},
+	{
+		name: 'cat-scrunkly',
+		alt: 'a scrunkly looking cat',
+		src: catScrunkly
+	},
+	{
+		name: 'meow-chemical-romance',
+		alt: 'an emo cat ',
+		src: meowChemicalRomance
+	},
+	{
+		name: 'cat-fabulous',
+		alt: 'a cat on holiday with pink sunglasses, probably in hawaii',
+		src: catFabulous
+	},
+	{
+		name: 'bicep-curl',
+		alt: 'a cat holding dumbells',
+		src: bicepCurl
+	},
+	{
+		name: 'getting-swole',
+		alt: 'a cat with a very heavy dumbell',
+		src: gettingSwole
+	},
+	{
+		name: 'not-amused',
+		alt: 'a cat that is not amused',
+		src: notAmused
+	},
+	{
+		name: 'u-wot-m8',
+		alt: 'a cat in a fighting stance',
+		src: uWotM8
+	},
+	{
+		name: 'cat-admire',
+		alt: 'a cat looking at you with admiration',
+		src: catAdmire
+	},
+	{
+		name: 'cat-WFH',
+		alt: 'a cat working, he is enjoying his new mechanical keyboard',
+		src: catWFH
+	},
+	{
+		name: 'chef-cat',
+		alt: 'a small cat with a wooden spoon and chef hat',
+		src: chefCat
+	},
+	{
+		name: 'cat-stare',
+		alt: 'a cat staring',
+		src: catStare
+	},
+	{
+		name: 'cat-heck',
+		alt: 'a cat basically saying oh nooo with its paws on its face',
+		src: catHeck
+	},
+	{
+		name: 'cat-angry',
+		alt: 'an angry cat',
+		src: catAngry
+	},
+	{
+		name: 'cat-cozy',
+		alt: 'a cozy cat - my spirit animal',
+		src: catCozy
+	},
+	{
+		name: 'cat-mechanic',
+		alt: 'a cat holding a spanner',
+		src: catMechanic
+	},
+	{
+		name: 'sous-chef',
+		alt: 'the assistant chef cat with a hat',
+		src: sousChef
+	},
+	{
+		name: 'cat-inspect',
+		alt: 'a cat looking intensely',
+		src: catInspect
+	},
+	{
+		name: 'cat-observe',
+		alt: 'a cat semi-smiling, no thoughts head empty',
+		src: catObserve
+	},
+	{
+		name: 'cat-shook',
+		alt: 'a surprised cat',
+		src: catShook
+	},
+	{
+		name: 'senor-cheeto',
+		alt: 'a large cat with cheeto dust on its paws',
+		src: senorCheeto
+	},
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,84 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { emojis, type Emoji } from '../lib/data/emojis';
+
+	let selectedEmoji: Emoji = emojis[0];
+	let message = '';
+
+	function handleKeydown(index: number) {
+		selectedEmoji = emojis[index];
+		message = '';
+	}
+
+	async function copyToClipboard(text: string) {
+		try {
+			await navigator.clipboard.writeText(text);
+			message = 'copied to clipboard! ✅';
+		} catch (err) {
+			console.error('Failed to copy: ', err);
+		}
+	}
+
+	function downloadImage(imageUrl: string) {
+		const link = document.createElement('a');
+		const fileExtension = imageUrl.split('.').pop()?.split('?')[0] || 'png';
+
+		link.href = imageUrl;
+		link.download = `${selectedEmoji.name}.${fileExtension}`;
+
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}
+
+	const ACTION_BTN_CLASSES =
+		'rounded-md px-2 py-1 text-xs text-violet-50/70 bg-violet-50/5 hover:bg-violet-50/10 focus:outline-none focus:ring-1 focus:ring-violet-50';
+</script>
+
+<svelte:head>
+	<title>slack cats 🐈</title>
+	<meta name="description" content="A variety of silly cat emojis for Slack!" />
+</svelte:head>
+
+<div class="h-screen bg-gray-800 flex flex-col items-center pb-12">
+	<h1 class="text-2xl font-semibold mt-10 text-violet-400">slack cats</h1>
+	<p class="text-violet-50/80 max-w-[40ch] text-center text-xs mt-2">
+		✨ the emoji directory you didn't know you needed ✨
+	</p>
+
+	{#if selectedEmoji}
+		<img
+			src={selectedEmoji.src}
+			alt={selectedEmoji.alt}
+			class="h-32 w-32 md:h-52 md:w-52 object-cover mt-8"
+		/>
+		<div class="flex flex-col gap-2 items-center mt-1">
+			<p class="text-violet-50 text-lg">{`${selectedEmoji.name}`}</p>
+			<div class="flex gap-1">
+				<button class={ACTION_BTN_CLASSES} on:click={() => copyToClipboard(selectedEmoji.name)}
+					>copy name</button
+				>
+				<button class={ACTION_BTN_CLASSES} on:click={() => downloadImage(selectedEmoji.src)}
+					>download image</button
+				>
+				<!-- Added Download Button -->
+			</div>
+			<p class="text-xs text-gray-400">{message}</p>
+		</div>
+	{/if}
+	<div
+		class="flex w-5/6 justify-center flex-wrap gap-3 mt-12 max-h-[400px] md:max-h-fit overflow-y-scroll rounded-md bg-violet-400/5 py-4"
+	>
+		{#each emojis as emoji, index}
+			<button
+				on:click={() => handleKeydown(index)}
+				on:keydown={() => handleKeydown(index)}
+				on:mouseover={() => handleKeydown(index)}
+				on:focus={() => handleKeydown(index)}
+				class="border border-violet-400/60 flex gap-1 items-center p-2 rounded-md w-fit bg-violet-950/20 focus:outline-none focus:ring-1 focus:ring-violet-300"
+			>
+				<img src={emoji.src} alt={emoji.alt} class="object-cover h-[32px] w-[32px]" />
+				<p class="text-sm text-violet-50">{`:${emoji.name}:`}</p>
+			</button>
+		{/each}
+	</div>
+</div>
